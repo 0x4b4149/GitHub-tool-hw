a1edf1d3b99417432fcd9e35daaaa92b8d1569d7
0x4b4149 <yuki.yksu@gmail.com>
2026-04-09 14:57:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <!-- 標題：有點像店的招牌 -->
+    <h1>🔍 GitHub 偵察機</h1>
+    
+    <!-- 輸入框：讓你寫下要搜尋的名字 -->
+    <!-- v-model 就像是把輸入的內容放進盒子裡 -->
+    <input 
+      v-model="名字盒子" 
+      placeholder="請輸入 GitHub 帳號" 
+    />
+    
+    <!-- 按鈕：按下後會觸發搜尋功能 -->
+    <button @click="搜尋功能">搜尋</button>
+    
+    <!-- 顯示結果區域：找到後會顯示在這裡 -->
+    <div v-if="照片盒子" class="result">
+      <!-- 這裡會顯示找到的頭像 -->
+      <img :src="照片盒子" alt="頭像" />
+      <!-- 這裡會顯示用户名 -->
+      <p>{{ 名字盒子 }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// 引入 ref（像一個裝資料的盒子）
+import { ref } from 'vue'
+
+// 建立一個盒子叫做「名字盒子」，一開始是空的
+const 名字盒子 = ref('')
+
+// 建立一個盒子叫做「照片盒子」，一開始也是空的
+const 照片盒子 = ref('')
+
+// 搜尋功能的工廠
+function 搜尋功能() {
+  // 用 GitHub API 去抓資料
+  fetch('https://api.github.com/users/' + 名字盒子.value)
+    .then(response => response.json())
+    .then(data => {
+      // 把找到的頭像網址放進照片盒子
+      照片盒子.value = data.avatar_url
+    })
+}
+
+// 把這些功能都傳給模板使用
+export default {
+  setup() {
+    return {
+      名字盒子,
+      照片盒子,
+      搜尋功能
+    }
+  }
+}
+</script>
+
+<style>
+/* 讓畫面好看一點的化妝師（CSS） */
+.container {
+  text-align: center;
+  padding: 50px;
+  font-family: sans-serif;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+  width: 200px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+.result {
+  margin-top: 30px;
+}
+
+.result img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 3px solid #333;
+}
+</style>
